7de4b2d8b1ed10d9c1f391fafcbb7da7c4287549
omkarvijay6 <omkarvijay6@gmail.com>
2014-11-08 12:38:09 +0530

    added get route about_us for about us

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#show'
+  get '/about_us/', to: 'home#about_us'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
